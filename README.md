c16e192b5cb783a3d32e92721ac5ea68a77c17ce
leopico <linkoko.peceng@gmail.com>
2024-01-15 23:04:32 +0630

    add readme.md

---
+++ README.md
@@ -0,0 +1,31 @@
+This is a [Counter pj](https://github.com/leopico/Solana-Bootcamp-Counter.git) project
+
+![(Test for counter)](https://ibb.co/MMzZLfY)
+
+What I learned from this course:
+
+- How to set up for account
+- How to set up instructions data
+- How to set up for account and instructions data in testing
+
+### Prerequisites
+
+**Cargo & Solana**
+
+### Cloning the repository
+
+```shell
+git clone https://github.com/leopico/Solana-Bootcamp-Counter.git
+```
+
+### Install packages
+
+```shell
+cargo check
+```
+
+### Start for testing
+
+```shell
+cargo test -- --nocapture
+```
---
+++ src/lib.rs
@@ -0,0 +1,130 @@
+use std::u32;
+
+use borsh::{BorshDeserialize, BorshSerialize};
+use borsh_derive::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+use crate::instructions::CounterInstructions;
+pub mod instructions;
+
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
+pub struct CounterAccount {
+    pub counter: u32,
+}
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instructions_data: &[u8],
+) -> ProgramResult {
+    msg!("Counter program entry point!");
+
+    let instructions: CounterInstructions = CounterInstructions::unpack(instructions_data)?;
+
+    let account_iter = &mut accounts.iter();
+    let account = next_account_info(account_iter)?;
+
+    let mut counter_account = CounterAccount::try_from_slice(&account.data.borrow())?;
+
+    match instructions {
+        CounterInstructions::Increment(args) => {
+            counter_account.counter += args.value;
+        }
+        CounterInstructions::Decrement(args) => {
+            if args.value > counter_account.counter {
+                counter_account.counter = 0;
+            } else {
+                counter_account.counter -= args.value;
+            }
+        }
+        CounterInstructions::Update(args) => {
+            counter_account.counter = args.value;
+        }
+        CounterInstructions::Reset => {
+            counter_account.counter = 0;
+        }
+    }
+
+    counter_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use solana_program::{clock::Epoch, pubkey::Pubkey};
+    use std::mem;
+
+    #[test]
+    fn test_counter() {
+        let program_id = Pubkey::default();
+        let key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = vec![0; mem::size_of::<u32>()];
+        let owner = Pubkey::default();
+
+        let account = AccountInfo::new(
+            &key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+
+        let accounts = vec![account];
+
+        let mut increment_instruction_data: Vec<u8> = vec![0];
+        let mut decrement_instruction_data: Vec<u8> = vec![1];
+        let mut update_instruction_data: Vec<u8> = vec![2];
+        let reset_instruction_data: Vec<u8> = vec![3];
+
+        let increment_value = 10u32;
+        increment_instruction_data.extend_from_slice(&increment_value.to_le_bytes());
+        process_instruction(&program_id, &accounts, &increment_instruction_data).unwrap();
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            10
+        );
+
+        let decrement_value = 20u32;
+        decrement_instruction_data.extend_from_slice(&decrement_value.to_le_bytes());
+        process_instruction(&program_id, &accounts, &decrement_instruction_data).unwrap();
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            0
+        );
+
+        let update_value = 33u32;
+        update_instruction_data.extend_from_slice(&update_value.to_le_bytes());
+        process_instruction(&program_id, &accounts, &update_instruction_data).unwrap();
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            33
+        );
+
+        process_instruction(&program_id, &accounts, &reset_instruction_data).unwrap();
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            0
+        );
+    }
+}
